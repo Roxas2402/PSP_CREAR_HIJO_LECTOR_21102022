a63a44ee56ea799640b12f45b902af2bb9142d46
roxas <jeropastrana@gmail.com>
2022-10-21 16:24:03 +0200

    aaa

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         String ficheroNombre = sc.nextLine();
 
         File fichero = new File(ficheroNombre);
 
         if (fichero.exists()) {
+            FileReader fr = new FileReader(fichero);
+            BufferedReader br = new BufferedReader(fr);
 
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
         } else {
             throw new FileNotFoundException();
         }
