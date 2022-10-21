7de44ebaebfc87699a3b17a85a7e275f80efe924
roxas <jeropastrana@gmail.com>
2022-10-21 16:20:53 +0200

    aaa

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         String ficheroNombre = sc.nextLine();
 
@@ -12,7 +13,7 @@ public class Main {
         if (fichero.exists()) {
 
         } else {
-
+            throw new FileNotFoundException();
         }
     }
 }
